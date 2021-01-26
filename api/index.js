3092a4b4ea58981747c25633b9e0299ecba234b7
kimhakmin <kimhm260@gmail.com>
2021-01-26 17:54:24 +0900

    test

--- api/index.js
+++ api/index.js
@@ -5,13 +5,19 @@ const express = require('express')
 
 const app = express()
 
-app.post('/callback/apple/', (req, res) => {
+app.get('/callback/apple/', (req, res) => {
   // let body = '';
   // request.on('code', (code) => {
 
   // })
   console.log(req)
   console.log(res)
+})
+
+app.get('/api', (req, res) => {
+  console.log(req)
+  console.log(res)
+  console.log('hello')
 })
 
 module.exports = {
